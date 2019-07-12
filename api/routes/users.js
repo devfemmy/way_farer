8f64195a21e6b1f3e0208085d8c81f2442e9e304
Femi Bincom <femi.bincom@gmail.com>
2019-07-12 18:22:49 +0100

    feature(user-signup-endpoint):add user sign up endpoint [Finishes #167196093]

---
+++ api/routes/users.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/get', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling Get requests'
+    });
+});
+
+//user sign up 
+
+router.post('/auth/signup', (req, res, next) => {
+    const cred = {
+        email:req.body.email,
+        password:req.body.password,
+        first_name: req.body.first_name,
+        last_name: req.body.last_name
+    }
+    res.status(200).json({
+        status: 'success',
+        data: {
+            user_id: Number,
+            is_admin: Boolean,
+            token: String
+        }
+    });
+});
+
+//user sign in
+
+// router.post('/auth/signin', (req, res, next) => {
+//     res.status(200).json({
+//         status: 'success',
+//         data: {
+//             user_id: Number
+//         }
+//     });
+// });
+
+
+//user delete
+
+
+module.exports = router;
